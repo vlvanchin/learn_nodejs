6d216150e97625cd437b10d59d4bd61b3b437b99
vlvanchin <vlvanchin+work@gmail.com>
2022-09-11 00:35:07 +0800

    updated some node file
    Signed-off-by: vlvanchin <vlvanchin+work@gmail.com>

---
+++ first-servers/mysite.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var backend = http.createServer(function(req, res) {
+    var q = url.parse(req.url, true);
+//    console.log (q);
+    var filename = "." + q.pathname;
+
+
+    if (filename == "./") {
+        filename = "./index";
+    } else if (filename.lastIndexOf(".") != -1 && filename.lastIndexOf(".") > 0) {
+        filename = filename.slice(0,filename.lastIndexOf("."));
+    } 
+
+    filename = filename + ".html";
+    fs.readFile (filename, function(err,data) {
+        if (err) {
+            res.writeHead (404, {'Content-Type':'text/html'});
+            return res.end("404 file not found");
+        }
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        console.log ("... Incoming request: " + req.url);
+        return res.end();
+    });
+});
+
+backend.listen(8000);
+
+console.log("server is listenning on port 8000");
